1c01aae311fd6647125b96db18526670d463b30c
Alex Galvin <alex.v.galvin@gmail.com>
2025-06-02 04:24:59 -0400

    chore: clippy

--- crates/librqbit/src/session.rs
+++ crates/librqbit/src/session.rs
@@ -998,11 +998,8 @@ impl Session {
             return Ok(None);
         }
 
-        fn check_valid(pb: &PathBuf) -> anyhow::Result<()> {
-            if pb.components().into_iter().any(|x| match x {
-                Component::Normal(_) => false,
-                _ => true,
-            }) {
+        fn check_valid(pb: &Path) -> anyhow::Result<()> {
+            if pb.components().any(|c| !matches!(c, Component::Normal(_)))  {
                 bail!("path traversal in torrent name detected")
             }
             Ok(())
